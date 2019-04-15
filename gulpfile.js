8e26e59ac400efc790dc32eec67ff55483b504d0
zsp <1048365746@qq.com>
2019-04-15 09:44:42 +0800

    节能二

--- gulpfile.js
+++ gulpfile.js
@@ -4,3 +4,27 @@ let scss = require('gulp-sass');
 let js = require('gulp-uglify');
 let fs = require('fs');
 let path = require('path')
+
+gulp.task('css', () => {
+    return gulp.src('./src/css/*.css')
+        .pipe(css())
+        .pipe(concat('main.min.css'))
+        .pipe(dest('./dist/css'))
+})
+
+gulp.js('js', () => {
+    return gulp.src('./src/js/*.js')
+        .pipe(js())
+        .pipe(concat('main.min.js'))
+        .pipe(dest('./dist/js'))
+})
+
+gulp.img('img', () => {
+    return gulp.src('src/img/*.jpg')
+        .pipe(img())
+        .pipe(dest('./dist/img'))
+})
+
+gulp.webserver('server', () => {
+
+})
